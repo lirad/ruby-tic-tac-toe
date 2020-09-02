09bfdade358af6c1f6ad422bed8dc25a96bc8c8b
Diego Lira <diegoalira@gmail.com>
2020-09-02 06:19:08 -0300

    Player token and board display

---
+++ lib/board.rb
@@ -0,0 +1,9 @@
+class Board
+  
+  
+
+  def initialize
+ 
+  end
+
+end
---
+++ lib/players.rb
@@ -0,0 +1,22 @@
+class Players
+  @@token_array = %w[x X o O]  
+  attr_reader :name
+  attr_reader :token
+
+  def initialize(name, token)
+    @name = name
+    @token = token
+  end
+
+  def self.validate_token?(token = nil)
+    if @@token_array.any?(token)
+       true
+    else
+      false
+    end  
+  end
+
+  def display
+    puts "#{@name} and #{@token}"
+  end
+end
