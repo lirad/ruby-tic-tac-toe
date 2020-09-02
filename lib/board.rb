a5b7930313aafa2e2acb35eef1882fe8dba2774f
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-09-02 12:50:06 +0200

    intiated board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,30 @@
 class Board
-  def initialize; end
+  attr_accessor :@board_display
+  def initialize
+    @board_display = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  end
+
+
+  def game
+    5.times do
+    def player1
+      change_board("X","player1")
+    end
+    def player2
+      change_board("O","player2")
+    end
+    player1
+    player2
+  end
+  end
+
+
+  def position_validation?(number)
+    board_flatten = $board_display.flatten 
+    if board_flatten.any?(number) 
+    else   
+      p "Invalid position"
+      get_player_move
+    end 
+  end  
 end
